c75fc3449c6083023708aa1e1dab5db4ab26e850
angga <68156108+anggavirgi@users.noreply.github.com>
2023-08-30 09:42:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. Buatlah sebuah function dengan nama changeWord yang berfungsi untuk menggantikan sebuah kata didalam sebuah kalimat. Function ini akan menerima 3 parameter yaitu selectedText, changedText, text
+
+function changeWord(selectedText, changedText, text) {
+  
+}
